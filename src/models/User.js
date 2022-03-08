d025f6631c74ef8f7f0060794970e09837724ba2
Asmaa <Asmaa99musse@gmail.com>
2022-03-08 21:28:17 +0000

    refactored code

--- src/models/User.js
+++ src/models/User.js
@@ -1,58 +1,54 @@
-import mongoose from "mongoose";
-import {hash, compare} from "bcrypt";
-
-const {Schema, model} = mongoose;
+const { Schema, model } = require("mongoose");
+const { hash, compare } = require("bcrypt");
 
 const userSchema = new Schema(
-	{
-		username: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		firstName: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		lastName: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-			match: [/.+@.+\..+/, "Must use a valid email address"],
-		},
-		password: {
-			type: String,
-			required: true,
-			minlength: 8,
-		},
-	},
-	// set this to use virtual below
-	{
-		toJSON: {
-			virtuals: true,
-		},
-	}
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
 );
 
 // hash user password
 userSchema.pre("save", async function (next) {
-	if (this.isNew || this.isModified("password")) {
-		const saltRounds = 10;
-		this.password = await hash(this.password, saltRounds);
-	}
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await hash(this.password, saltRounds);
+  }
 
-	next();
+  next();
 });
 
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
-	return compare(password, this.password);
+  return compare(password, this.password);
 };
 
 export const User = model("User", userSchema);
--- src/resolvers/addUser.js
+++ src/resolvers/addUser.js
@@ -1,15 +1,15 @@
-import User from "../models/index.js";
-import {signToken} from "../utils/auth.js";
+const User = require("../models/index.js");
+const { signToken } = require("../utils/auth.js");
 
-export const addUser = async (_, {userInput}) => {
-	try {
-		const newUser = await User.create(userInput);
+export const addUser = async (_, { userInput }) => {
+  try {
+    const newUser = await User.create(userInput);
 
-		return {
-			token: signToken(newUser),
-			user: newUser,
-		};
-	} catch (error) {
-		console.log(`[ERROR]: Failed to create User || ${error.message}`);
-	}
+    return {
+      token: signToken(newUser),
+      user: newUser,
+    };
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create User || ${error.message}`);
+  }
 };
