8eb896e97776fe4f52576d339357850fc4c41373
Darion Richards <darionrichards@outlook.com>
2022-03-09 14:35:26 +0000

    Created login resolver and exported through mutations

---
+++ src/resolvers/login.js
@@ -0,0 +1,30 @@
+import {AuthenticationError} from "apollo-server-errors";
+import User from "../models/index.js";
+import {signToken} from "../utils/auth.js";
+
+export const login = async (_, {input}) => {
+	try {
+		console.log("Resolver hit");
+		const userFromDb = await User.findOne({email: input.email});
+
+		if (!userFromDb) {
+			console.log("[ERROR]: Failed to login || Cannot find user");
+			throw new AuthenticationError("Failed to login");
+		}
+
+		const correctPw = await userFromDb.isCorrectPassword(input.password);
+
+		if (!correctPw) {
+			console.log("[ERROR]: Failed to login || Incorrect password");
+			throw new AuthenticationError("Failed to login");
+		}
+
+		console.log("Successfully logged in");
+		return {
+			token: signToken(userFromDb),
+			user: userFromDb,
+		};
+	} catch (error) {
+		console.log(`[ERROR]: Failed to login || ${error.message}`);
+	}
+};
