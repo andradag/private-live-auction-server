9fee2312bf1e58b34f711186976287e66e2cee71
Darion Richards <darionrichards@outlook.com>
2022-03-09 14:34:32 +0000

    Updated schema to accept login mutation
    - created loginInput as arguments

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,32 +1,38 @@
-import { gql } from "apollo-server";
+import {gql} from "apollo-server";
 
 export const typeDefs = gql`
-  type User {
-    _id: ID!
-    username: String!
-    firstName: String!
-    lastName: String!
-    email: String!
-  }
+	type User {
+		_id: ID!
+		username: String!
+		firstName: String!
+		lastName: String!
+		email: String!
+	}
 
-  type Auth {
-    token: ID!
-    user: User
-  }
+	type Auth {
+		token: ID!
+		user: User
+	}
 
-  type Query {
-    getSingleUser: User
-  }
+	type Query {
+		getSingleUser: User
+	}
 
-  input UserInput {
-    username: String!
-    firstName: String!
-    lastName: String!
-    email: String!
-    password: String!
-  }
+	input UserInput {
+		username: String!
+		firstName: String!
+		lastName: String!
+		email: String!
+		password: String!
+	}
 
-  type Mutation {
-    addUser(userInput: UserInput!): Auth
-  }
+	input LoginInput {
+		email: String!
+		password: String!
+	}
+
+	type Mutation {
+		addUser(userInput: UserInput!): Auth
+		login(input: LoginInput!): Auth
+	}
 `;
