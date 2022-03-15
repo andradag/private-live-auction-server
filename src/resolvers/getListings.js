29fd7e6c588c52f04d7721bff2f6a4415e9f85c6
Darion Richards <darionrichards@outlook.com>
2022-03-15 20:16:52 +0000

    Populate categories

--- src/resolvers/getListings.js
+++ src/resolvers/getListings.js
@@ -1,28 +1,28 @@
-const { ApolloError } = require("apollo-server-errors");
-const { Listing } = require("../models");
+const {ApolloError} = require("apollo-server-errors");
+const {Listing} = require("../models");
 
-const getListings = async (_, { status, category }, context) => {
-  try {
-    let listings = [];
-    if (status || category) {
-      const options = [];
-      if (status) {
-        options.push({ status });
-      }
+const getListings = async (_, {status, category}, context) => {
+	try {
+		let listings = [];
+		if (status || category) {
+			const options = [];
+			if (status) {
+				options.push({status});
+			}
 
-      if (category) {
-        options.push({ category });
-      }
+			if (category) {
+				options.push({category});
+			}
 
-      listings = await Listing.find({ $and: options });
-    } else {
-      listings = await Listing.find({});
-    }
+			listings = await Listing.find({$and: options}).populate("category");
+		} else {
+			listings = await Listing.find({}).populate("category");
+		}
 
-    return listings;
-  } catch (error) {
-    console.log(`[ERROR]: Failed to get listings | ${error.message}`);
-  }
+		return listings;
+	} catch (error) {
+		console.log(`[ERROR]: Failed to get listings | ${error.message}`);
+	}
 };
 
 module.exports = getListings;
