bf6b65f5ce7bbee44bb831d3cf8e9bae9e016d41
surajverma2587 <surajverma2587@gmail.com>
2022-03-21 20:03:49 +0000

    update listing for houses

---
+++ src/models/Bid.js
@@ -0,0 +1,22 @@
+const { Schema } = require("mongoose");
+
+const bidSchema = {
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+};
+
+const schema = new Schema(bidSchema, {
+  toJSON: {
+    getters: true,
+  },
+  id: true,
+});
+
+module.exports = schema;
