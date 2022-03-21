75cc28dbd034f1f83f511a123aa122cc6ab15bd7
Darion Richards <darionrichards@outlook.com>
2022-03-21 14:47:05 +0000

    Updated Listing Schema, Model and Resolver to store time when a bid was placed

--- src/models/Listing.js
+++ src/models/Listing.js
@@ -2,64 +2,64 @@ const {Schema, model} = require("mongoose");
 const Category = require("./Category");
 
 const listingSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    //   images: [
-    //       {
-    //           type: String,
-    //       }
-    //   ],
-    category: {
-      type: Schema.Types.ObjectId,
-      ref: Category,
-      required: true,
-    },
-    reserveAmount: {
-      type: Number,
-      required: true,
-    },
-    startingBid: {
-      type: Number,
-      required: true,
-    },
-    status: {
-      type: String,
-      required: true,
-      default: "Upcoming",
-      enum: ["Live", "Upcoming"],
-    },
-    createdBy: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    bids: [
-      {
-        user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-        amount: { type: Number },
-      },
-    ],
-  },
-  {
-    toJSON: {
-      getters: true,
-      virtuals: true,
-    },
-  }
+	{
+		title: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		//   images: [
+		//       {
+		//           type: String,
+		//       }
+		//   ],
+		category: {
+			type: Schema.Types.ObjectId,
+			ref: Category,
+			required: true,
+		},
+		reserveAmount: {
+			type: Number,
+			required: true,
+		},
+		startingBid: {
+			type: Number,
+			required: true,
+		},
+		status: {
+			type: String,
+			required: true,
+			default: "Upcoming",
+			enum: ["Live", "Upcoming"],
+		},
+		createdBy: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		bids: [
+			{
+				user: {type: Schema.Types.ObjectId, required: true, ref: "User"},
+				amount: {type: Number},
+				bidTime: {type: String},
+			},
+		],
+	},
+	{
+		toJSON: {
+			getters: true,
+			virtuals: true,
+		},
+	}
 );
 
 listingSchema.virtual("currentBid").get(function () {
-  return (this?.bids && this?.bids.length && this?.bids.pop()) || null;
+	return (this?.bids && this?.bids.length && this?.bids.pop()) || null;
 });
-  
 
 const Listing = model("Listing", listingSchema);
 
--- src/schema/index.js
+++ src/schema/index.js
@@ -1,90 +1,92 @@
-const { gql } = require("apollo-server");
+const {gql} = require("apollo-server");
 
 const typeDefs = gql`
-  type User {
-    id: ID!
-    username: String!
-    firstName: String!
-    lastName: String!
-    imageUrl: String!
-    email: String!
-    isAdmin: Boolean!
-    savedListings: [Listing]
-  }
+	type User {
+		id: ID!
+		username: String!
+		firstName: String!
+		lastName: String!
+		imageUrl: String!
+		email: String!
+		isAdmin: Boolean!
+		savedListings: [Listing]
+	}
 
-  type Listing {
-    _id: ID!
-    title: String!
-    description: String!
-    category: Category!
-    reserveAmount: Float!
-    startingBid: Float!
-    status: String!
-    currentBid: Bid
-    bids: [Bid]
-  }
+	type Listing {
+		_id: ID!
+		title: String!
+		description: String!
+		category: Category!
+		reserveAmount: Float!
+		startingBid: Float!
+		status: String!
+		currentBid: Bid
+		bids: [Bid]
+	}
 
-  type Category {
-    _id: ID!
-    title: String
-  }
+	type Category {
+		_id: ID!
+		title: String
+	}
 
-  type Auth {
-    token: ID!
-    user: User
-  }
+	type Auth {
+		token: ID!
+		user: User
+	}
 
-  type Bid {
-    amount: Float!
-    user: User!
-    listingId: ID!
-  }
+	type Bid {
+		amount: Float!
+		user: User!
+		listingId: ID!
+		bidTime: String!
+	}
 
-  type Query {
-    getSingleUser(userId: ID!): User
-    getAllCategories: [Category]
-    getSingleListing(_id: ID!): Listing
-    getListings(status: String, category: ID): [Listing]
-  }
+	type Query {
+		getSingleUser(userId: ID!): User
+		getAllCategories: [Category]
+		getSingleListing(_id: ID!): Listing
+		getListings(status: String, category: ID): [Listing]
+	}
 
-  input UserInput {
-    username: String!
-    firstName: String!
-    lastName: String!
-    imageUrl: String
-    email: String!
-    password: String!
-  }
+	input UserInput {
+		username: String!
+		firstName: String!
+		lastName: String!
+		imageUrl: String
+		email: String!
+		password: String!
+	}
 
-  input LoginInput {
-    email: String!
-    password: String!
-  }
+	input LoginInput {
+		email: String!
+		password: String!
+	}
 
-  input ListingInput {
-    title: String!
-    description: String!
-    category: String!
-    reserveAmount: Float!
-    startingBid: Float!
-  }
+	input ListingInput {
+		title: String!
+		description: String!
+		category: String!
+		reserveAmount: Float!
+		startingBid: Float!
+	}
 
-  input AddBidInput {
-    amount: Float!
-    listingId: ID!
-  }
+	input AddBidInput {
+		amount: Float!
+		listingId: ID!
+		bidTime: String!
+	}
 
-  type Mutation {
-    addUser(userInput: UserInput!): Auth
-    login(input: LoginInput!): Auth
-    addListing(listingInput: ListingInput!): Listing
-    saveAListing(input: ID!): User
-    addBid(input: AddBidInput!): Bid!
-  }
+	type Mutation {
+		addUser(userInput: UserInput!): Auth
+		login(input: LoginInput!): Auth
+		addListing(listingInput: ListingInput!): Listing
+		saveAListing(input: ID!): User
+		addBid(input: AddBidInput!): Bid!
+	}
 
-  type Subscription {
-    auctionBid(listingId: ID!): Bid
-  }
+	type Subscription {
+		auctionBid(listingId: ID!): Bid
+	}
 `;
 
 module.exports = typeDefs;
