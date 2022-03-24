fbff64ffb20ff09f26fc9e83f3924410884cca5a
surajverma2587 <surajverma2587@gmail.com>
2022-03-24 18:49:56 +0000

    fix autciton status subscription

--- src/resolvers/addBid.js
+++ src/resolvers/addBid.js
@@ -1,39 +1,39 @@
-const {Listing} = require("../models");
+const { Listing } = require("../models");
 const pubsub = require("./pubSub");
 
-const addBid = async (_, {input}, {user}) => {
-	try {
-		if (user) {
-			const {status} = await Listing.findById(input.listingId);
+const addBid = async (_, { input }, { user }) => {
+  try {
+    if (user) {
+      const { status } = await Listing.findById(input.listingId);
 
-			if (status === "Live") {
-				await Listing.findByIdAndUpdate(input.listingId, {
-					$push: {
-						bids: {
-							amount: input.amount,
-							user: user.id,
-							bidTime: input.bidTime,
-						},
-					},
-				});
+      if (status === "Live") {
+        await Listing.findByIdAndUpdate(input.listingId, {
+          $push: {
+            bids: {
+              amount: input.amount,
+              user: user.id,
+              bidTime: input.bidTime,
+            },
+          },
+        });
 
-				const bid = {
-					amount: input.amount,
-					user,
-					listingId: input.listingId,
-					bidTime: input.bidTime,
-				};
+        const bid = {
+          amount: input.amount,
+          user,
+          listingId: input.listingId,
+          bidTime: input.bidTime,
+        };
 
-				pubsub.publish("AUCTION_BID", {
-					auctionBid: {bid, status},
-				});
+        pubsub.publish("AUCTION_BID", {
+          auctionBid: { bid, status },
+        });
 
-				return {bid, status};
-			}
-		}
-	} catch (error) {
-		console.log(`[ERROR]: Failed to add bid | ${error.message}`);
-	}
+        return { bid, status };
+      }
+    }
+  } catch (error) {
+    console.log(`[ERROR]: Failed to add bid | ${error.message}`);
+  }
 };
 
 module.exports = addBid;
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -9,39 +9,40 @@ const getSingleListing = require("./getSingleListing");
 const addBid = require("./addBid");
 const deleteListing = require("./deleteListing");
 const controlListing = require("./controlListing");
-const {withFilter} = require("graphql-subscriptions");
+const { withFilter } = require("graphql-subscriptions");
 
 const pubsub = require("./pubSub");
 
 const resolvers = {
-	Query: {
-		getSingleUser,
-		getAllCategories,
-		getListings,
-		getSingleListing,
-	},
-	Mutation: {
-		addUser,
-		login,
-		addListing,
-		saveAListing,
-		addBid,
-		deleteListing,
-		controlListing,
-	},
-	Subscription: {
-		auctionBid: {
-			subscribe: withFilter(
-				() => pubsub.asyncIterator(["AUCTION_BID"]),
-				(payload, variables) => {
-					// Only push an update if the comment is on
-					// console.log(payload);
-					// the correct repository for this operation
-					return payload.auctionBid.listingId === variables.listingId;
-				}
-			),
-		},
-	},
+  Query: {
+    getSingleUser,
+    getAllCategories,
+    getListings,
+    getSingleListing,
+  },
+  Mutation: {
+    addUser,
+    login,
+    addListing,
+    saveAListing,
+    addBid,
+    deleteListing,
+    controlListing,
+  },
+  Subscription: {
+    auctionBid: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(["AUCTION_BID"]),
+        (payload, variables) => {
+          // Only push an update if the comment is on
+          // console.log(payload);
+          // console.log(variables);
+          // the correct repository for this operation
+          return payload.auctionBid.listingId === variables.listingId;
+        }
+      ),
+    },
+  },
 };
 
 module.exports = resolvers;
